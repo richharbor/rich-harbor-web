f7a0b42e6b2ab6486fd4926474826a5d205928ef
Ahmar Ansari <85979277+Ahmar512@users.noreply.github.com>
2025-08-23 21:27:29 +0530

    added mailing function in contact us page

--- components/Pages/ContactUs/page.tsx
+++ components/Pages/ContactUs/page.tsx
@@ -5,6 +5,7 @@ import { Mail, Phone } from "lucide-react";
 import { useState } from "react";
 import { motion } from 'framer-motion'
 import { CircleCheckBig } from 'lucide-react';
+import axios from 'axios';
 
 import {
     Dialog,
@@ -36,6 +37,27 @@ type FormData = {
     message: string;
 };
 
+const baseURl = process.env.NEXT_PUBLIC_API_BASE_URL;
+
+function convertToHtmlForm(data: Record<string, string>): string {
+  let html = `<div style="font-family: Arial, sans-serif; line-height: 1.6;">`;
+  html += `<h2>New Form Submission</h2>`;
+  html += `<table border="1" cellpadding="8" cellspacing="0" style="border-collapse: collapse; width: 100%;">`;
+
+  for (const [key, value] of Object.entries(data)) {
+    html += `
+      <tr>
+        <td style="font-weight: bold; text-transform: capitalize;">${key}</td>
+        <td>${value}</td>
+      </tr>
+    `;
+  }
+
+  html += `</table></div>`;
+  return html;
+}
+
+
 export default function ContactForm() {
     const [formData, setFormData] = useState<FormData>({
         name: "",
@@ -59,11 +81,25 @@ export default function ContactForm() {
     };
 
     // Handle submit
-    const handleSubmit = (e: React.FormEvent) => {
+    const  handleSubmit = async (e: React.FormEvent) => {
         e.preventDefault();
         // console.log("Form Data Submitted:", formData);
 
         // TODO: send data to API
+
+        const emailHTML = convertToHtmlForm(formData);
+        console.log(emailHTML)
+
+        const email = {
+            to: formData.email,
+            subject:formData.subject + " - " + formData.name,
+            content: emailHTML,
+            isHtml: true,
+        }
+
+        const response = await axios.post(`${baseURl}/email/send`, email);
+
+        console.log(response.data);
 
 
         setFormData({
