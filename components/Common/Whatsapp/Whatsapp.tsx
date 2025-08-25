412dd51ea9eebb54e7e8827bf8ae5c013a59abf3
prabhat <prabhat@rhinontech.com>
2025-08-25 21:46:35 +0530

    new changes

---
+++ components/Common/Whatsapp/Whatsapp.tsx
@@ -0,0 +1,21 @@
+import React from "react";
+
+const Whatsapp = () => {
+  return (
+    <div>
+      <a
+        href="https://wa.me/919211265558"
+        target="_blank"
+        rel="noopener noreferrer"
+      >
+        <img
+          src="https://upload.wikimedia.org/wikipedia/commons/thumb/5/5e/WhatsApp_icon.png/640px-WhatsApp_icon.png"
+          alt="WhatsApp"
+          width={50}
+        />
+      </a>
+    </div>
+  );
+};
+
+export default Whatsapp;
