197904709310055ffbbc0d6883a6d2206d4c8b1d
Ahmar Ansari <85979277+Ahmar512@users.noreply.github.com>
2025-08-29 12:00:01 +0530

    added some animation

--- components/Pages/Home/JoinLeague/JoinLeague.tsx
+++ components/Pages/Home/JoinLeague/JoinLeague.tsx
@@ -1,4 +1,5 @@
 import { GlowingEffect } from "@/components/ui/glowing-effect";
+import { cn } from "@/lib/utils";
 import { BadgeIndianRupee, ChartBarBig, IndianRupee, LockKeyhole, Zap } from "lucide-react";
 
 
@@ -6,22 +7,22 @@ import { BadgeIndianRupee, ChartBarBig, IndianRupee, LockKeyhole, Zap } from "lu
 const features = [
     {
         title: "Low ticket size",
-        icon: <BadgeIndianRupee className="size-10 max-sm:size-10" />, // Placeholder for icon path
+        icon: <BadgeIndianRupee className="size-full text-purple-900" />, // Placeholder for icon path
         description: "A minimum of ₹10,000 is all it takes for you to start investing in private opportunities",
     },
     {
         title: "Bank-level security",
-        icon: <LockKeyhole className="size-10 max-sm:size-10" />, // Placeholder for icon path
+        icon: <LockKeyhole className="size-full text-purple-900" />, // Placeholder for icon path
         description: "Strict policies are enforced to prevent unauthorized revelation of your sensitive personal information",
     },
     {
         title: "Research-driven",
-        icon: <ChartBarBig className="size-10 max-sm:size-10" />,
+        icon: <ChartBarBig className="size-full text-purple-900" />,
         description: "Access curated financial reports by analyst for informed investment decisions",
     },
     {
         title: "Fast & Simple",
-        icon: <Zap className="size-10 max-sm:size-10" />, // Placeholder for icon path
+        icon: <Zap className="size-full text-purple-900 " />, // Placeholder for icon path
         description: "Hassle-free investment experience. Invest in just three steps - confirm, order, and pay",
     },
 ];
@@ -66,14 +67,55 @@ const GridItem = ({ area, icon, title, description }: GridItemProps) => {
     );
 };
 
+const Feature = ({
+    title,
+    description,
+    icon,
+    index,
+}: {
+    title: string;
+    description: string;
+    icon: React.ReactNode;
+    index: number;
+}) => {
+    return (
+        <div
+            className={cn(
+                "flex flex-col lg:border-r  py-10 relative group/feature dark:border-neutral-800",
+                (index === 0 || index === 4) && "lg:border-l dark:border-neutral-800",
+                index < 4 && "lg:border-b dark:border-neutral-800"
+            )}
+        >
+            {index < 4 && (
+                <div className="opacity-0 group-hover/feature:opacity-100 ease-in-out transition-all duration-300 absolute inset-0 h-full w-full bg-gradient-to-t from-neutral-100 dark:from-purple-900/25 to-transparent pointer-events-none" />
+            )}
+            {index >= 4 && (
+                <div className="opacity-0 group-hover/feature:opacity-100 ease-in-out transition-all duration-300 absolute inset-0 h-full w-full bg-gradient-to-b from-neutral-100 dark:from-purple-900/25 to-transparent pointer-events-none" />
+            )}
+            <div className="mb-4 w-fit group-hover/feature:scale-75 ease-in-out transition-all duration-500 h-12 relative z-10 px-10 text-neutral-600 dark:text-neutral-400">
+                {icon}
+            </div>
+            <div className="text-lg font-bold mb-2 relative z-10 px-10">
+                <div className="absolute  left-0 inset-y-0 h-6 group-hover/feature:h-8 group-hover/feature:-translate-y-1 transform-gpu w-1 rounded-tr-full ease-in-out rounded-br-full bg-neutral-300 dark:bg-neutral-700 group-hover/feature:bg-purple-900 transition-all duration-300 origin-center" />
+                <span className=" inline-block text-neutral-800 dark:text-neutral-100">
+                    {title}
+                </span>
+            </div>
+            <p className="text-sm text-neutral-600 dark:text-white/50 max-w-xs relative z-10 px-10">
+                {description}
+            </p>
+        </div>
+    );
+};
+
 
 export default function JoinLeague() {
     return (
         <div className="flex rounded-2xl flex-col gap-20 h-full mx-auto w-full lg:max-w-screen-xl px-2 lg:px-20 py-20 max-md:py-10">
             <h1 className="text-3xl mx-20 max-sm:mx-5 md:text-5xl text-center lg:text-6xl font-medium !leading-tight text-transparent bg-clip-text bg-gradient-to-b from-foreground to-neutral-400">User into a new era of private investments</h1>
 
-            <div className="grid grid-cols-4 max-lg:grid-cols-2 max-sm:grid-cols-1 px-3 gap-5">
-                {features.map((feature, index) => (
+            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4  relative z-10 py-10 max-w-7xl mx-auto">
+                {/* {features.map((feature, index) => (
                     <GridItem
                         area=""
                         icon={feature.icon}
@@ -81,7 +123,11 @@ export default function JoinLeague() {
                         description={feature.description}
                         key={index}
                     />
+                ))} */}
+                {features.map((feature, index) => (
+                    <Feature key={feature.title} {...feature} index={index} />
                 ))}
+
 
             </div>
         </div>
