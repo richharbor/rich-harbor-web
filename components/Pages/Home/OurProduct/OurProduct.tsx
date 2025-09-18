0a067db62d722c03e4ff56f73895c1c6ee29f4a9
Ahmar Ansari <ahmaransari29@gmail.com>
2025-09-18 17:48:49 +0530

    theme bug fixed

--- components/Pages/Home/OurProduct/OurProduct.tsx
+++ components/Pages/Home/OurProduct/OurProduct.tsx
@@ -15,7 +15,7 @@ export default function OurProduct() {
 
     return (
         <section className="max-w-7xl rounded-2xl h-full mx-auto w-full px-4 lg:px-20 md:py-20 py-5 bg-muted/30 relative overflow-hidden">
-            <div className="absolute inset-0 -z-10 h-full w-full bg-white dark:bg-black bg-[linear-gradient(to_right,#f0f0f0_1px,transparent_1px),linear-gradient(to_bottom,#f0f0f0_1px,transparent_1px)] dark:bg-[linear-gradient(to_right,#1f1f1f_1px,transparent_1px),linear-gradient(to_bottom,#1f1f1f_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_50%,#000_40%,transparent_100%)] rounded-lg"></div>
+            <div className="absolute inset-0 -z-10 h-full w-full bg-black bg-[linear-gradient(to_right,#1f1f1f_1px,transparent_1px),linear-gradient(to_bottom,#1f1f1f_1px,transparent_1px)] bg-[size:4rem_4rem] [mask-image:radial-gradient(ellipse_80%_50%_at_50%_50%,#000_40%,transparent_100%)] rounded-lg"></div>
             <div className='flex flex-col justify-center items-center gap-10 max-md:gap-12'>
                 <div>
                     <div className='flex flex-col gap-5 items-center'>
@@ -52,7 +52,7 @@ export default function OurProduct() {
                 </div>
                 <div className='grid grid-cols-4 min-h-[8rem] gap-5 w-full max-md:grid-cols-2'>
                     <CometCard rotateDepth={6} translateDepth={10} >
-                        <div onClick={()=> route.push("/allstocks")} className='bg-card cursor-pointer h-[8rem] rounded-xl flex gap-3 justify-center items-center py-3 px-5 max-sm:px-2 group hover:bg-rich-violet  transition-all duration-300 ease-in-out '>
+                        <div onClick={()=> route.push("/#hot-ipo")} className='bg-card cursor-pointer h-[8rem] rounded-xl flex gap-3 justify-center items-center py-3 px-5 max-sm:px-2 group hover:bg-rich-violet  transition-all duration-300 ease-in-out '>
                             <LineChart className='size-10 group-hover:text-black max-sm:size-8' />
                             <h1 className='font-bold max-md:font-normal group-hover:text-black '>Unlisted Shares</h1>
                         </div>
