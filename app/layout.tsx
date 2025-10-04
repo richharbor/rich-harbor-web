7ee207df5155b55785046ea84d77344b83040eac
prabhat <prabhat@rhinontech.com>
2025-10-04 13:05:48 +0530

    new changes

--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,7 @@ import "@/app/globals.css";
 import { Inter } from "next/font/google";
 import "@radix-ui/themes/styles.css";
 import type { Metadata } from "next";
+import Script from "next/script"; // 👈 important
 import Footer from "../components/Common/Footer/Footer";
 import Navbar from "@/components/Common/Navbar/Navbar";
 import Whatsapp from "@/components/Common/Whatsapp/Whatsapp";
@@ -24,6 +25,22 @@ export default function RootLayout({
 }) {
   return (
     <html lang="en" suppressHydrationWarning>
+      <head>
+        {/* Google Analytics (gtag.js) */}
+        <Script
+          src="https://www.googletagmanager.com/gtag/js?id=G-S23L52FW0T"
+          strategy="afterInteractive"
+          async
+        />
+        <Script id="google-analytics" strategy="afterInteractive">
+          {`
+            window.dataLayer = window.dataLayer || [];
+            function gtag(){dataLayer.push(arguments);}
+            gtag('js', new Date());
+            gtag('config', 'G-S23L52FW0T');
+          `}
+        </Script>
+      </head>
       <body className={inter.className}>
         <Navbar />
         <div className="mx-auto items-center max-md:px-4 flex flex-col relative">
